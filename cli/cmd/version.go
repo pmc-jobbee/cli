a3a6729d38b9c0c15668b775f1a9ff5bbdc66312
Richard Vaughan <richard@purplemonkeys.co.uk>
2022-06-25 09:20:25 +0100

    Small structural tweaks.

--- cli/cmd/version.go
+++ cli/cmd/version.go
@@ -2,15 +2,17 @@ package cmd
 
 import (
 	"fmt"
-  
+
 	"github.com/spf13/cobra"
-  )
-  
+
+	"github.com/pmc-jobbee/cli/cli/app"
+)
+
 var cmdVersion = &cobra.Command{
-    Use:   "version",
-    Short: "Print app version",
-    Long: `Print app version.`,
-    Run: func(cmd *cobra.Command, args []string) {
-      fmt.Println("version: v0.0.0")
-    },
-  }
+	Use:   "version",
+	Short: "Print app version",
+	Long:  `Print app version.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("%s v%s-%s (Built: %s)\n", app.AppName, app.AppVersion, app.AppGitHash, app.AppBuildDate)
+	},
+}
